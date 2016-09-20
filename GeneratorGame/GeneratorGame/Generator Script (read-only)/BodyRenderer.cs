66bfe766ab73bec72f879ffe7fe661896ccbb2d9
tenedria <tylasya@hotmail.com>
2016-09-20 18:31:02 -0400

    Add files via upload

---
+++ GeneratorGame/GeneratorGame/Generator Script (read-only)/BodyRenderer.cs
@@ -0,0 +1,200 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BodyRenderer : MonoBehaviour 
+{
+// ================================================
+// 		BODY RENDERER
+//		*spawns the body parts and changes textures
+// ================================================
+
+//Series of numbers behaving like DNA
+	[Header("SELECTOR")]
+	public int HeadNumber;
+	public int LegNumber;
+	public int TailNumber;
+	public int ManeNumber;
+	public int EarNumber;
+	public int HornNumber;
+	public int PeltSelector;
+	public int EyeSelector;
+
+//Editable Arrays of body parts
+	[Header("GAMEOBJECTS")]
+	public GameObject[] Heads;
+	public GameObject[] Legs;
+	public GameObject[] Tails;
+	public GameObject[] Manes;
+	public GameObject[] Ears;
+	public GameObject[] Horns;
+
+//Object to spawn and possition relative to the base body
+	private GameObject Head;
+	private Vector3 HeadPos = new Vector3(0,1.43f,0.03f);
+	private GameObject Leg;
+	private Vector3 LegPos = new Vector3(0,0,0);
+	private GameObject Tail;
+	private Vector3 TailPos = new Vector3(0,0,0);
+	private GameObject Mane;
+	private Vector3 ManePos = new Vector3(0,1.551f,0f);
+	private GameObject Ear;
+	private Vector3 EarPos = new Vector3(0,1.23f,-0.1f);
+	private GameObject Horn;
+	private Vector3 HornPos = new Vector3(0,1.40f,0);
+
+//temporary identifier to retrieve the spawned object after it is spawned
+	//GameObjects
+	private GameObject tempHead;
+	private GameObject tempLeg;
+	private GameObject tempTail;
+	private GameObject tempMane;
+	private GameObject tempEar;
+	private GameObject tempHorn;
+
+
+//Functions to spawn each object
+	void spawnAll(GameObject tempObject, GameObject bodyPart, Vector3 bodyPos)
+	{
+		tempObject = Instantiate (bodyPart) as GameObject;
+		tempObject.transform.parent = gameObject.transform;
+		tempObject.transform.localPosition = bodyPos;
+		tempObject.SetActive (true);
+	}
+
+
+// ================================================
+// 		TEXTURES
+//		*changes textures
+// ================================================
+	//Number to identify the materials
+
+	private int eyeNumeral;
+	private int peltNumeral;
+
+	[Header("TEXTURES")]
+	//Eyes
+	public Material[] Eyes;
+	private Material[] HeadTextures;
+	private Material newEye;
+	//Pelt
+	public Material[] Pelts;
+	private Material newPelt;
+
+	//function renames gameobjects to their tag's name
+	void RenameBody(string BodyParttoRename)
+	{
+		GameObject[] bodyparts = GameObject.FindGameObjectsWithTag (BodyParttoRename);
+		foreach (GameObject bodypart in bodyparts) 
+		{
+			bodypart.name = BodyParttoRename;
+		}
+	}
+
+
+	// ** Essential BugFixing Code
+	//this code identifies the order of the materials linked on the head (because there is 2)
+	void materialTest()
+	{
+		RenameBody("Head");
+		tempHead = gameObject.transform.FindChild ("Head").gameObject;
+		//Debug.Log ("material 0 is : " + tempHead.transform.GetComponent<MeshRenderer> ().materials [0].mainTexture.name);
+		if (tempHead.transform.GetComponent<MeshRenderer> ().materials [0].mainTexture.name == "Pelt_test") 
+		{
+			PeltSwitch (1,0);
+		}
+
+		else 
+		{
+			PeltSwitch (0,1);
+		}
+	}
+	//This code switches the current materials to the appropriate ones
+	void PeltSwitch(int eyeno, int peltno)
+	{
+		//EYE SWITCH
+		newEye = Eyes [EyeSelector];
+		HeadTextures = tempHead.transform.GetComponent<MeshRenderer>().materials;
+		HeadTextures[eyeno].mainTexture = newEye.mainTexture;
+
+		//PElT SWITCH
+		newPelt = Pelts [PeltSelector];
+		HeadTextures[peltno].mainTexture = newPelt.mainTexture;
+		gameObject.transform.GetComponent<MeshRenderer> ().materials[0].mainTexture = newPelt.mainTexture;
+
+		//findLeg
+		RenameBody("Feet");
+		tempLeg = gameObject.transform.FindChild("Feet").gameObject;
+		tempLeg.transform.GetComponent<MeshRenderer> ().material.mainTexture = newPelt.mainTexture;
+
+
+		if(Ear.gameObject.tag == "Ear")
+		{
+			RenameBody("Ear");
+			tempEar = gameObject.transform.FindChild("Ear").gameObject;
+			tempEar.transform.GetComponent<MeshRenderer> ().materials[0].mainTexture = newPelt.mainTexture;
+		}
+
+		if(Mane.gameObject.tag == "Mane")
+		{
+			RenameBody("Mane");
+			tempMane = gameObject.transform.FindChild("Mane").gameObject;
+			tempMane.transform.GetComponent<MeshRenderer> ().materials[0].mainTexture = newPelt.mainTexture;
+		}
+
+		if(Tail.gameObject.tag == "Tail")
+		{
+			RenameBody("Tail");
+			tempTail = gameObject.transform.FindChild("Tail").gameObject;
+			tempTail.transform.GetComponent<MeshRenderer> ().materials[0].mainTexture = newPelt.mainTexture;
+		}
+		if(Horn.gameObject.tag == "Horn")
+		{
+			RenameBody("Horn");
+			tempHorn = gameObject.transform.FindChild("Horn").gameObject;
+			//can change horn texture using code bellow
+			//tempHorn.transform.GetComponent<MeshRenderer> ().materials[0].mainTexture = newPelt.mainTexture;
+		}
+	}
+
+	void peltbodySwitch(GameObject bodyPelt)
+	{
+		bodyPelt.transform.GetComponent<MeshRenderer> ().material.mainTexture = newPelt.mainTexture;
+	}
+
+
+
+
+	public void generateBody()
+	{
+		//spawns all the body parts
+		spawnAll (tempHead,Head,HeadPos);
+		spawnAll (tempLeg, Leg, LegPos);
+		spawnAll (tempTail, Tail, TailPos);
+		spawnAll (tempMane, Mane, ManePos);
+		spawnAll (tempEar, Ear, EarPos);
+		spawnAll (tempHorn, Horn, HornPos);
+
+		//changes the material on the body parts
+		materialTest ();
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+		//identifies body parts
+		Head = Heads[HeadNumber];
+		Leg = Legs[LegNumber];
+		Tail = Tails[TailNumber];
+		Mane = Manes[ManeNumber];
+		Ear = Ears[EarNumber];
+		Horn = Horns [HornNumber];
+
+		generateBody ();
+	}
+
+
+	void Update()
+	{
+		;
+	}
+}
---
+++ GeneratorGame/GeneratorGame/Generator Script (read-only)/Generator.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Generator : MonoBehaviour 
+{
+	// code generates random numbers for BodyRenderer to use.
+	// **dependant on the presence of BodyRenderer
+
+
+	private int maxHead;
+	private int maxLeg;
+	private int maxTail;
+	private int maxMane;
+	private int maxEar;
+	private int maxHorn;
+	private int maxPelt;
+	private int maxEye;
+
+	public int NumberHead = 0;
+	public int NumberLeg = 0;
+	public int NumberTail = 0;
+	public int NumberMane = 0;
+	public int NumberEar = 0;
+	public int NumberHorn = 0;
+	public int NumberPelt = 0;
+	public int NumberEye = 0;
+
+	// Use this for initialization
+	void Start () 
+	{
+		//bellow code fetches how many body parts exist in the renderer code for each option
+		//I tried to make this into a function but it wouldn't calculate it
+
+		foreach (GameObject Head in gameObject.transform.GetComponent<BodyRenderer>().Heads) 
+		{
+			maxHead++;
+		}
+
+		foreach (GameObject Leg in gameObject.transform.GetComponent<BodyRenderer>().Legs) 
+		{
+			maxLeg++;
+		}
+
+		foreach (GameObject Tail in gameObject.transform.GetComponent<BodyRenderer>().Tails) 
+		{
+			maxTail++;
+		}
+		foreach (GameObject Mane in gameObject.transform.GetComponent<BodyRenderer>().Manes) 
+		{
+			maxMane++;
+		}
+		foreach (GameObject Ear in gameObject.transform.GetComponent<BodyRenderer>().Ears) 
+		{
+			maxEar++;
+		}
+		foreach (GameObject Horn in gameObject.transform.GetComponent<BodyRenderer>().Horns) 
+		{
+			maxHorn++;
+		}
+		foreach (Material Pelt in gameObject.transform.GetComponent<BodyRenderer>().Pelts) 
+		{
+			maxPelt++;
+		}
+		foreach (Material Eye in gameObject.transform.GetComponent<BodyRenderer>().Eyes) 
+		{
+			maxEye++;
+		}
+
+		//generates each number taking into account how many possibilities there is for each point
+		NumberHead = (Random.Range(0,maxHead));
+		NumberLeg = (Random.Range(0,maxLeg));
+		NumberTail = (Random.Range(0,maxTail));
+		NumberMane = (Random.Range(0,maxMane));
+		NumberEar = (Random.Range(0,maxEar));
+		NumberHorn = (Random.Range(0,maxHorn));
+		NumberPelt = (Random.Range(0,maxPelt));
+		NumberEye = (Random.Range(0,maxEye));
+
+		//reaches into the generator code to give the random generated numbers to generate
+		gameObject.transform.GetComponent<BodyRenderer>().HeadNumber = NumberHead;
+		gameObject.transform.GetComponent<BodyRenderer>().LegNumber = NumberLeg;
+		gameObject.transform.GetComponent<BodyRenderer>().TailNumber = NumberTail;
+		gameObject.transform.GetComponent<BodyRenderer>().ManeNumber = NumberMane;
+		gameObject.transform.GetComponent<BodyRenderer>().EarNumber = NumberEar;
+		gameObject.transform.GetComponent<BodyRenderer>().HornNumber = NumberHorn;
+		gameObject.transform.GetComponent<BodyRenderer>().PeltSelector = NumberPelt;
+		gameObject.transform.GetComponent<BodyRenderer>().EyeSelector = NumberEye;
+
+		//turns on the generator into the bodyrenderer to reload the new numbers
+		gameObject.transform.GetComponent<BodyRenderer>().enabled = true;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
